03f72021418fb4d7d114b8d8a51cf854ba1ce039
EvanilsonP <pevanilson99@gmail.com>
2023-01-06 11:42:38 -0300

    Creating a simple Web Server

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const fs = require('fs');
+const http = require('http');
 
-// // Blocking, synchronous way
-// // Reading the content in input.txt
-// const textIn = fs.readFileSync('input.txt', 'utf-8');
-// console.log(textIn);
-
-// const textOut = `What do I like? ${textIn}.\nCreated on ${Date.now()}`;
-// fs.writeFileSync('output.txt', textOut);
-// console.log('File written!');
-
-// Non-Blocking, asynchronous way
-fs.readFile('output.txt', 'utf-8', (err, data) => {
-    console.log(data);
+const server = http.createServer((req, res) => {
+    res.end('Hello from the server.');
 });
-console.log('Will read this file!');
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Listening to requests on port 8000');
+});
+
+// // // Blocking, synchronous way
+// // // Reading the content in input.txt
+// // const textIn = fs.readFileSync('input.txt', 'utf-8');
+// // console.log(textIn);
+
+// // const textOut = `What do I like? ${textIn}.\nCreated on ${Date.now()}`;
+// // fs.writeFileSync('output.txt', textOut);
+// // console.log('File written!');
+
+// // Non-Blocking, asynchronous way
+// fs.readFile('output.txt', 'utf-8', (err, data) => {
+//     console.log(data);
+// });
+// console.log('Will read this file!');
