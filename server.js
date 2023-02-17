fa7cc6182bd4f3cbd871555fbefbc5df237763dc
EvanilsonP <pevanilson99@gmail.com>
2023-02-17 12:08:02 -0300

    response object

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log('Request made.');
+    console.log(req.url, req.method);
+
+    // set header content type
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<p>Hello, ninjas!</p>');
+    res.write('<p>Hello, again!</p>');
+    res.end();
 });
 
 server.listen(3000, () => { console.log('Listening for requests on port 3000.') });
