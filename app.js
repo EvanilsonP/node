4fc5ebdac1b5bc85fc4b1ddcfe7c892a5c2ec0fc
EvanilsonP <pevanilson99@gmail.com>
2023-02-17 16:54:44 -0300

    ejs view engine

--- app.js
+++ app.js
@@ -2,22 +2,24 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// register view engine
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-    res.sendFile('./views/index.html', { root: __dirname });
+    res.render('index');
 });
 
 app.get('/about', (req, res) => {
-    res.sendFile('./views/about.html', { root: __dirname });
+    res.render('about');
 });
 
-// redirects
-app.get('/about-us', (req, res) => {
-    res.redirect('/about');
+app.get('/blogs/create', (req, res) => {
+    res.render('create');
 });
 
 // 404 pages
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', { root: __dirname });
+    res.status(404).render('404');
 });
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
