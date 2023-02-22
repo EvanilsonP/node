192deab3047bf23e2839b5877f1ce45896511ae4
EvanilsonP <pevanilson99@gmail.com>
2023-02-22 12:24:13 -0300

    refactoring

--- app.js
+++ app.js
@@ -1,36 +1,48 @@
 const express = require('express');
-const morgan = require('morgan');
+
+// express app
 const app = express();
-const port = 3000;
+
+// listen for requests
+app.listen(3000);
 
 // register view engine
 app.set('view engine', 'ejs');
-app.use(morgan('dev'));
 
 // middleware & static files
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    const blogs = [
-        { title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consecteur'},
-        { title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consecteur'},
-        { title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consecteur'},
-    ];
+app.use((req, res, next) => {
+  console.log('new request made:');
+  console.log('host: ', req.hostname);
+  console.log('path: ', req.path);
+  console.log('method: ', req.method);
+  next();
+});
 
-    res.render('index', { title: 'Home', blogs });
+app.use((req, res, next) => {
+  res.locals.path = req.path;
+  next();
+});
+
+app.get('/', (req, res) => {
+  const blogs = [
+    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+  ];
+  res.render('index', { title: 'Home', blogs });
 });
 
 app.get('/about', (req, res) => {
-    res.render('about', { title: 'About'} );
+  res.render('about', { title: 'About' });
 });
 
 app.get('/blogs/create', (req, res) => {
-    res.render('create', { title: 'Create a new Blog'});
+  res.render('create', { title: 'Create a new blog' });
 });
 
-// 404 pages
+// 404 page
 app.use((req, res) => {
-    res.status(404).render('404', { title: '404'});
+  res.status(404).render('404', { title: '404' });
 });
-
-app.listen(port, () => console.log(`Server listening on port ${port}`));
